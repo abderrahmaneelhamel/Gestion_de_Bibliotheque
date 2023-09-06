93c3418cc267b5836c8f668a3fd44fdac0bbc88a
abderrahmaneelhamel <112892674+abderrahmaneelhamel@users.noreply.github.com>
2023-09-06 10:57:26 +0100

    Add files via upload

---
+++ library.sql
@@ -0,0 +1,219 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 06 sep. 2023 à 11:57
+-- Version du serveur : 10.4.25-MariaDB
+-- Version de PHP : 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `library`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `ISBN` varchar(255) NOT NULL,
+  `quantity` int(255) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `author`, `ISBN`, `quantity`) VALUES
+(1, 'mobydick', 'me', '#3332', 3),
+(2, '1984', 'goerge', '#3213', 3);
+
+--
+-- Déclencheurs `books`
+--
+DELIMITER $$
+CREATE TRIGGER `available_tr` BEFORE INSERT ON `books` FOR EACH ROW BEGIN
+  IF NEW.status IS NULL THEN
+    SET NEW.status = 'available';
+  END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `borrow`
+--
+
+CREATE TABLE `borrow` (
+  `id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `reader_id` int(11) NOT NULL,
+  `borrow_date` date NOT NULL,
+  `return_date` date NOT NULL,
+  `status` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déclencheurs `borrow`
+--
+DELIMITER $$
+CREATE TRIGGER `quantity_tr` BEFORE INSERT ON `borrow` FOR EACH ROW BEGIN
+  UPDATE books SET books.quantity = books.quantity - 1 WHERE books.id = NEW.book_id;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `returns`
+--
+
+CREATE TABLE `returns` (
+  `id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `reader_id` int(11) NOT NULL,
+  `return_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `returns`
+--
+
+INSERT INTO `returns` (`id`, `book_id`, `reader_id`, `return_date`) VALUES
+(1, 1, 4, '2023-09-05'),
+(2, 1, 4, '2023-09-05');
+
+--
+-- Déclencheurs `returns`
+--
+DELIMITER $$
+CREATE TRIGGER `quantity_return_tr` BEFORE INSERT ON `returns` FOR EACH ROW BEGIN
+  UPDATE books SET books.quantity = books.quantity + 1 WHERE books.id = NEW.book_id;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
+(1, 'test', 'admin@gmail.com', 'password', 1),
+(2, 'abdo', 'abdo@gmail.com', 'password', 2),
+(4, 'me', 'me@gmail.com', 'password ', 2),
+(5, 'asiyya', 'asiyya@gmail.com', 'password ', 2);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `book_id` (`book_id`),
+  ADD KEY `reader_id` (`reader_id`);
+
+--
+-- Index pour la table `returns`
+--
+ALTER TABLE `returns`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `book_id` (`book_id`),
+  ADD KEY `reader_id` (`reader_id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `borrow`
+--
+ALTER TABLE `borrow`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `returns`
+--
+ALTER TABLE `returns`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD CONSTRAINT `borrow_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  ADD CONSTRAINT `borrow_ibfk_2` FOREIGN KEY (`reader_id`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `returns`
+--
+ALTER TABLE `returns`
+  ADD CONSTRAINT `returns_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  ADD CONSTRAINT `returns_ibfk_2` FOREIGN KEY (`reader_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
